d04c6044cc398adccc2e6bfe94697d1b6890058c
veronicah <veronicah.mbuvi@strathmore.edu>
2024-11-21 15:07:33 +0300

    changed the transaction

--- 12_database_transactions/script.sql
+++ 12_database_transactions/script.sql
@@ -1,14 +1,10 @@
--- Write your SQL code here
-
+-- Create a transaction that handles order placement
 START TRANSACTION;
 
--- Insert a new order
-INSERT INTO Order (order_quantity, order_date, customer_id)
-VALUES (5, CURRENT_DATE, 1);
+INSERT INTO Order (order_id, order_quantity, order_date, customer_id)
+VALUES (101, 2, '2024-11-20', 1);
 
--- Update product stock 
-UPDATE Product
-SET product_quantity = product_quantity - 5
-WHERE product_id = 101;
+INSERT INTO Payments (payment_id, order_id, amount_paid)
+VALUES (1001, 101, 200);
 
 COMMIT;
