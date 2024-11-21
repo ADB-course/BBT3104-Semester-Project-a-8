2b4646fc68e1d7f58338ce80cfba6b32e6250bb6
veronicah <veronicah.mbuvi@strathmore.edu>
2024-11-21 14:32:09 +0300

    made changes in check_constraints

--- 4_check_constraints/script.sql
+++ 4_check_constraints/script.sql
@@ -1,29 +1,43 @@
--- Write your SQL code here
- --Tables without Check Constraints (specify the storage engine and indexes for each)
+--Tables Without Check Constraints
+CREATE TABLE Department (
+    department_id INT AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(100) NOT NULL,
+    location VARCHAR(100) NOT NULL,
+    department_head VARCHAR(100) NOT NULL
+) ENGINE=InnoDB;
 
-CREATE TABLE products (
-    product_id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Employee (
+    employee_id INT AUTO_INCREMENT PRIMARY KEY,
+    employee_name VARCHAR(100) NOT NULL,
+    employee_role VARCHAR(100) NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES Department(department_id),
+    INDEX (department_id)
+) ENGINE=InnoDB;
+
+CREATE TABLE Customer (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(100) NOT NULL,
+    customer_email VARCHAR(100) NOT NULL UNIQUE
+) ENGINE=InnoDB;
+
+
+
+--Tables With Check Constraints
+
+CREATE TABLE Product (
+    product_id INT AUTO_INCREMENT PRIMARY KEY,
     product_name VARCHAR(100) NOT NULL,
-    category_id INT NOT NULL,
-    stock_quantity INT NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    INDEX (category_id)
+    product_quantity INT NOT NULL CHECK (product_quantity >= 0),
+    product_price DECIMAL(10, 2) NOT NULL CHECK (product_price > 0)
 ) ENGINE=InnoDB;
 
---Tables with Check Constraints (specify the storage engine and indexes for each)
-CREATE TABLE employees (
-    employee_id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    age INT CHECK (age >= 18),
-    salary DECIMAL(10, 2) CHECK (salary > 0)
-) ENGINE=InnoDB;
-
-CREATE TABLE orders (
-    order_id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Order(
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_quantity INT NOT NULL CHECK (order_quantity > 0),
     order_date DATE NOT NULL,
     customer_id INT NOT NULL,
-    total_price DECIMAL(10, 2),
-    CHECK (total_price >= 0)
+    FOREIGN KEY (customer_id) REFERENCES Customer(customer_id),
+    CHECK (order_quantity <= (SELECT product_quantity FROM Product WHERE product_id = order_id)),
+    INDEX (order_date)
 ) ENGINE=InnoDB;
-
