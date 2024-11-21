c7572fd30fd3c5428a46a055a9e39ae8cb9ff793
veronicah <veronicah.mbuvi@strathmore.edu>
2024-11-21 14:15:48 +0300

    created procedurs fo the product_id

--- 9_stored_routines_as_procedures/script.sql
+++ 9_stored_routines_as_procedures/script.sql
@@ -1,1 +1,13 @@
 -- Write your SQL code here
+DELIMITER //
+
+CREATE PROCEDURE calculate_total_sales(IN product_id INT, OUT total_sales DECIMAL(10, 2))
+BEGIN
+    SELECT SUM(quantity * price) INTO total_sales
+    FROM order_details
+    WHERE product_id = product_id;
+END;
+//
+
+DELIMITER ;
+
