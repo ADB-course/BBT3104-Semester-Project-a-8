9a0a41ca74e0f0e9108f3bd3c7b5e9839e014818
veronicah <veronicah.mbuvi@strathmore.edu>
2024-11-21 14:44:28 +0300

    created a transaction

--- 12_database_transactions/script.sql
+++ 12_database_transactions/script.sql
@@ -1,1 +1,14 @@
 -- Write your SQL code here
+
+START TRANSACTION;
+
+-- Insert a new order
+INSERT INTO Order (order_quantity, order_date, customer_id)
+VALUES (5, CURRENT_DATE, 1);
+
+-- Update product stock after order is placed
+UPDATE Product
+SET product_quantity = product_quantity - 5
+WHERE product_id = 101;
+
+COMMIT;
