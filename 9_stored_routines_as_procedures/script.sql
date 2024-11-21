22807e1cdcb6102fd88bedc794379c1a5a6f7683
veronicah <veronicah.mbuvi@strathmore.edu>
2024-11-21 15:03:42 +0300

    added code in routine_as_procedures

--- 9_stored_routines_as_procedures/script.sql
+++ 9_stored_routines_as_procedures/script.sql
@@ -1,15 +1,24 @@
---calculates the total order value for a specific customer:
-
+-- Procedure to generate a monthly sales report
 DELIMITER //
-
-CREATE PROCEDURE calc_total_order_value(IN customer_id INT, OUT total_value DECIMAL(10, 2))
+CREATE PROCEDURE GenerationOfMonthlySalesReport()
 BEGIN
-    SELECT SUM(order_quantity * product_price) INTO total_value
+    SELECT 
+        p.product_name, 
+        SUM(o.order_quantity) AS total_sales, 
+        SUM(o.order_quantity * p.product_price) AS total_revenue
     FROM Order o
-    JOIN Product p ON o.order_id = p.product_id
-    WHERE o.customer_id = customer_id;
-END;
-//
-
+    JOIN Product p ON o.product_id = p.product_id
+    WHERE o.order_date BETWEEN CURDATE() - INTERVAL 1 MONTH AND CURDATE()
+    GROUP BY p.product_name;
+END //
 DELIMITER ;
- 
+
+-- Procedure to retrieve employee performance by department
+DELIMITER //
+CREATE PROCEDURE RetrievalOfEmployeePerformance(department_id INT)
+BEGIN
+    SELECT employee_name, performance_rating
+    FROM Employee
+    WHERE department_id = department_id;
+END //
+DELIMITER ;
