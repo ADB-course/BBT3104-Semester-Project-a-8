4a0a40ff567eb13abf0a9c9cb1f4df82ca447d58
veronicah <veronicah.mbuvi@strathmore.edu>
2024-11-21 15:00:16 +0300

    changed code on normal_triggers

--- 3_normal_triggers/script.sql
+++ 3_normal_triggers/script.sql
@@ -1,18 +1,23 @@
--- Write your SQL code here
-
-
+-- Trigger to set order status to "Pending Payment" after a new order is created
 DELIMITER //
-
-CREATE TRIGGER trg_update_order_date
-AFTER UPDATE ON `Order`
+CREATE TRIGGER trg_set_order_status
+AFTER INSERT ON Order
 FOR EACH ROW
 BEGIN
-    IF NEW.order_quantity != OLD.order_quantity THEN
-        UPDATE `Order`
-        SET order_date = CURRENT_DATE
-        WHERE order_id = NEW.order_id;
-    END IF;
-END;
-//
+    UPDATE Order
+    SET order_status = 1 -- Pending Payment
+    WHERE order_id = NEW.order_id;
+END //
+DELIMITER ;
 
+-- Trigger to validate employee performance rating before insert/update
+DELIMITER //
+CREATE TRIGGER trg_check_employee_performance
+BEFORE INSERT OR UPDATE ON Employee
+FOR EACH ROW
+BEGIN
+    IF NEW.performance_rating > 5 OR NEW.performance_rating < 1 THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Performance rating must be between 1 and 5';
+    END IF;
+END //
 DELIMITER ;
