fdb9bc09ad92b9a47e41fa2faf510caae15485c9
veronicah <veronicah.mbuvi@strathmore.edu>
2024-11-21 15:01:44 +0300

    added code in temporal_triggers

--- 5_temporal_triggers/script.sql
+++ 5_temporal_triggers/script.sql
@@ -1,14 +1,24 @@
 -- Write your SQL code here
---update order_date every time an order is modified:
+-- Trigger to update inventory levels after order dispatch
 DELIMITER //
-
-CREATE TRIGGER trg_update_order_date_on_modify
-BEFORE UPDATE ON `Order`
+CREATE TRIGGER trg_update_inventory_after_dispatch
+AFTER UPDATE ON `Order`
 FOR EACH ROW
 BEGIN
-    SET NEW.order_date = CURRENT_DATE;
-END;
-//
-
+    IF NEW.dispatch_date IS NOT NULL AND OLD.dispatch_date IS NULL THEN
+        UPDATE Product
+        SET product_quantity = product_quantity - NEW.order_quantity
+        WHERE product_id = NEW.product_id;
+    END IF;
+END //
 DELIMITER ;
 
+-- Trigger to archive old orders before deletion
+DELIMITER //
+CREATE TRIGGER trg_archive_old_orders
+BEFORE DELETE ON `Order`
+FOR EACH ROW
+BEGIN
+    INSERT INTO OrderArchive SELECT * FROM `Order` WHERE order_id = OLD.order_id;
+END //
+DELIMITER ;
