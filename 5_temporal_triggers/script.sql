9631936a6b129bb654a92c226152650469196e97
veronicah <veronicah.mbuvi@strathmore.edu>
2024-11-21 14:39:16 +0300

    added data on temporal triggers

--- 5_temporal_triggers/script.sql
+++ 5_temporal_triggers/script.sql
@@ -1,12 +1,14 @@
 -- Write your SQL code here
+--update order_date every time an order is modified:
 DELIMITER //
 
-CREATE TRIGGER trg_update_order_timestamp
-BEFORE UPDATE ON orders
+CREATE TRIGGER trg_update_order_date_on_modify
+BEFORE UPDATE ON `Order`
 FOR EACH ROW
 BEGIN
-    SET NEW.updated_at = NOW();
+    SET NEW.order_date = CURRENT_DATE;
 END;
 //
 
 DELIMITER ;
+
