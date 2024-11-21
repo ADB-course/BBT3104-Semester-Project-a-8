b8a12cfb4ba2e1707963871d21b747a61e0d189e
veronicah <veronicah.mbuvi@strathmore.edu>
2024-11-21 14:49:56 +0300

    added code to functions

--- 12_database_transactions/script.sql
+++ 12_database_transactions/script.sql
@@ -6,7 +6,7 @@ START TRANSACTION;
 INSERT INTO Order (order_quantity, order_date, customer_id)
 VALUES (5, CURRENT_DATE, 1);
 
--- Update product stock after order is placed
+-- Update product stock 
 UPDATE Product
 SET product_quantity = product_quantity - 5
 WHERE product_id = 101;
