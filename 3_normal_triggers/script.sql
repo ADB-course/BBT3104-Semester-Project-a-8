6e1b60b6eec33fb1ba6c3cf31dff42cdcebe718f
veronicah <veronicah.mbuvi@strathmore.edu>
2024-11-21 14:01:30 +0300

    added data on normal triggers

--- 3_normal_triggers/script.sql
+++ 3_normal_triggers/script.sql
@@ -1,1 +1,14 @@
 -- Write your SQL code here
+
+DELIMITER //
+
+CREATE TRIGGER trg_update_stock
+AFTER UPDATE ON products
+FOR EACH ROW
+BEGIN
+    INSERT INTO stock_log (product_id, old_quantity, new_quantity, updated_at)
+    VALUES (OLD.product_id, OLD.stock_quantity, NEW.stock_quantity, NOW());
+END;
+//
+
+DELIMITER ;
