3453d4777e72dd64f81b0cd7d877fa185b625de0
veronicah <veronicah.mbuvi@strathmore.edu>
2024-11-21 14:14:07 +0300

    created trigger on the orders

--- 5_temporal_triggers/script.sql
+++ 5_temporal_triggers/script.sql
@@ -1,1 +1,12 @@
 -- Write your SQL code here
+DELIMITER //
+
+CREATE TRIGGER trg_update_order_timestamp
+BEFORE UPDATE ON orders
+FOR EACH ROW
+BEGIN
+    SET NEW.updated_at = NOW();
+END;
+//
+
+DELIMITER ;
