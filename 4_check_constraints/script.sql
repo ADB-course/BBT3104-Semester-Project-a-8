cb84ef922949e0be39c98bac4130aa2168265109
veronicah <veronicah.mbuvi@strathmore.edu>
2024-11-21 13:58:23 +0300

    added data on check_constraints

--- 4_check_constraints/script.sql
+++ 4_check_constraints/script.sql
@@ -1,1 +1,14 @@
 -- Write your SQL code here
+CREATE TABLE products (
+    product_id INT PRIMARY KEY AUTO_INCREMENT,
+    product_name VARCHAR(100) NOT NULL,
+    category_id INT NOT NULL,
+    stock_quantity INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    INDEX (category_id)
+) ENGINE=InnoDB;
+
+CREATE TABLE categories (
+    category_id INT PRIMARY KEY AUTO_INCREMENT,
+    category_name VARCHAR(50) NOT NULL
+) ENGINE=InnoDB;
