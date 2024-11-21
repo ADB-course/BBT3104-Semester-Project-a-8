ae2d1021c4c4cae11656629137d26ffe4292cd85
veronicah <veronicah.mbuvi@strathmore.edu>
2024-11-21 14:05:54 +0300

    added check_constraints

--- 4_check_constraints/script.sql
+++ 4_check_constraints/script.sql
@@ -1,4 +1,6 @@
 -- Write your SQL code here
+ --Tables without Check Constraints (specify the storage engine and indexes for each)
+
 CREATE TABLE products (
     product_id INT PRIMARY KEY AUTO_INCREMENT,
     product_name VARCHAR(100) NOT NULL,
@@ -8,7 +10,20 @@ CREATE TABLE products (
     INDEX (category_id)
 ) ENGINE=InnoDB;
 
-CREATE TABLE categories (
-    category_id INT PRIMARY KEY AUTO_INCREMENT,
-    category_name VARCHAR(50) NOT NULL
+--Tables with Check Constraints (specify the storage engine and indexes for each)
+CREATE TABLE employees (
+    employee_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    age INT CHECK (age >= 18),
+    salary DECIMAL(10, 2) CHECK (salary > 0)
 ) ENGINE=InnoDB;
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    order_date DATE NOT NULL,
+    customer_id INT NOT NULL,
+    total_price DECIMAL(10, 2),
+    CHECK (total_price >= 0)
+) ENGINE=InnoDB;
+
