b279d6bd5a3b069e6b433ac599f64495a13cf7a1
veronicah <veronicah.mbuvi@strathmore.edu>
2024-11-21 14:42:34 +0300

    added data on views

--- 11_views/script.sql
+++ 11_views/script.sql
@@ -1,1 +1,10 @@
 -- Write your SQL code here
+-- see the order details along with customer information:
+CREATE VIEW customer_order_summary AS
+SELECT 
+    c.customer_name,
+    o.order_id,
+    o.order_quantity,
+    o.order_date
+FROM Customer c
+JOIN `Order` o ON c.customer_id = o.customer_id;
--- 9_stored_routines_as_procedures/script.sql
+++ 9_stored_routines_as_procedures/script.sql
@@ -5,10 +5,11 @@ DELIMITER //
 CREATE PROCEDURE calc_total_order_value(IN customer_id INT, OUT total_value DECIMAL(10, 2))
 BEGIN
     SELECT SUM(order_quantity * product_price) INTO total_value
-    FROM Order 
+    FROM Order o
     JOIN Product p ON o.order_id = p.product_id
     WHERE o.customer_id = customer_id;
 END;
 //
 
 DELIMITER ;
+ 
