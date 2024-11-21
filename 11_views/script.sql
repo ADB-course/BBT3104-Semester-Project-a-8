9b59d7be8ea30277ecd55c5ee2886fe320ae97e6
veronicah <veronicah.mbuvi@strathmore.edu>
2024-11-21 15:05:47 +0300

    added a a table in views

--- 11_views/script.sql
+++ 11_views/script.sql
@@ -1,10 +1,14 @@
 -- Write your SQL code here
--- see the order details along with customer information:
-CREATE VIEW customer_order_summary AS
-SELECT 
-    c.customer_name,
-    o.order_id,
-    o.order_quantity,
-    o.order_date
-FROM Customer c
-JOIN `Order` o ON c.customer_id = o.customer_id;
+-- see the order details along with customer information(history):
+
+CREATE VIEW ClientOrderHistory AS
+SELECT c.customer_name, o.order_id, o.order_date, o.order_quantity, p.product_name
+FROM Order o
+JOIN Customer c ON o.customer_id = c.customer_id
+JOIN Product p ON o.product_id = p.product_id;
+
+-- View to show employee performance overview
+CREATE VIEW EmployeePerformanceOverview AS
+SELECT e.employee_name, e.employee_role, e.performance_rating, d.department_name
+FROM Employee e
+JOIN Department d ON e.department_id = d.department_id;
